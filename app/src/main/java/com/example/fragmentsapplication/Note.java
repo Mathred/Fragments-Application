ecd74d50ab658c8d2879a7cc9b9d48459d5d225f
Mathred <temhbluturpa@gmail.com>
2021-05-16 23:20:01 +0300

    Created Note class

---
+++ app/src/main/java/com/example/fragmentsapplication/Note.java
@@ -0,0 +1,14 @@
+package com.example.fragmentsapplication;
+
+import java.text.SimpleDateFormat;
+
+public class Note {
+    private String name;
+    private String description;
+    private SimpleDateFormat dateCreated;
+
+    public Note() {
+    }
+
+
+}
