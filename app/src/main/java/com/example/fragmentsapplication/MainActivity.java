b8295ab48fe286143ee3c8a4700996218e0710a1
Mathred <temhbluturpa@gmail.com>
2021-05-20 02:52:55 +0300

    Added styles in themes.xml Changed application theme in AndroidManifest.xml + suppressed backup attribute Added ActionBar menu layout (action_bar_menu.xml) Added coordinatorlayout to build.gradle Added layouts for ActionBar (activity_main.xml) and Content (content_main.xml) Added base init for ActionBar in MainActivity Added addFragment method Changed  <item name="windowNoTitle">true</item> in <style name="AppTheme.NoActionBar">

--- app/src/main/java/com/example/fragmentsapplication/MainActivity.java
+++ app/src/main/java/com/example/fragmentsapplication/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.fragmentsapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,69 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        initToolbar();
+
+    }
+
+    private void initToolbar() {
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_settings:
+                addFragment(new SettingsFragment());
+                return true;
+            case R.id.action_main:
+                addFragment(new MainFragment());
+                return true;
+            case R.id.action_favorite:
+                addFragment(new FavoritesFragment());
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void addFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.action_bar_menu, menu);
+        MenuItem search = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) search.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //TODO to replace Toast for search action
+                Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //TODO to add search action
+                return true;
+            }
+        });
+
+        return true;
     }
 }
