bc2244f7e5a5d1071849adb9424ec4298689fd01
Mathred <temhbluturpa@gmail.com>
2021-05-29 12:26:31 +0300

    Fixed comments

--- app/src/main/java/com/example/fragmentsapplication/MainFragment.java
+++ app/src/main/java/com/example/fragmentsapplication/MainFragment.java
@@ -2,19 +2,15 @@ package com.example.fragmentsapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 public class MainFragment extends Fragment {
 
@@ -35,18 +31,18 @@ public class MainFragment extends Fragment {
             activity.getMenuInflater().inflate(R.menu.popup, popupMenu.getMenu());
             Menu menu = popupMenu.getMenu();
             menu.findItem(R.id.item2_popup).setVisible(false);
-            menu.add(0,123456,12,R.string.new_menu_item_added);
+            menu.add(0, 123456, 12, R.string.new_menu_item_added);
             popupMenu.setOnMenuItemClickListener(item -> {
                 int id = item.getItemId();
                 switch (id) {
                     case R.id.item1_popup:
-                        Toast.makeText(getContext(),"Chosen popup item 1",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "Chosen popup item 1", Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.item2_popup:
-                        Toast.makeText(getContext(),"Chosen popup item 2",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "Chosen popup item 2", Toast.LENGTH_SHORT).show();
                         return true;
                     case 123456:
-                        Toast.makeText(getContext(),"Chosen new popup item added",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "Chosen new popup item added", Toast.LENGTH_SHORT).show();
                         return true;
                 }
                 return true;
